7651981bde55e9fba4829cf4af98fe2ecc196ac9
okai <michokai@gmail.com>
2024-10-20 07:25:30 +0900

    TeplateView  1

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import IndexView
-
+from django.views.generic.base import TemplateView
 app_name = 'library'
 urlpatterns = [
     path('index/',IndexView.as_view(), name='index'),
+    path('home/',TemplateView.as_view(template_name='home.html'), name='home'),
 ]
