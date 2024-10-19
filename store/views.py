7aeb064e85b100e1bfb16a3e7c6838d235e9c65e
okai <michokai@gmail.com>
2024-10-19 15:30:31 +0900

    クラスベースビュー（２）

--- store/views.py
+++ store/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.views.generic.base import (
+from django.views.generic.base import(
     View,
 )
+from . import forms
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'index.html')
+        book_form=forms.BookForm()
+        return render(request, 'index.html', context={'book_form':book_form,})
